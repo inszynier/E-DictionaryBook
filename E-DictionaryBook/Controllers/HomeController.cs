b6c1e5161b71e04c740aec20e28d0e74745510c0
Kamil <Kamil@test.pl>
2018-01-16 22:05:12 +0100

    First change. Add test value to homecontroller.

--- E-DictionaryBook/Controllers/HomeController.cs
+++ E-DictionaryBook/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace E_DictionaryBook.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. Test";
 
             return View();
         }
