335575988c53443a61c8f577eb2fc853c6bbf32d
Kamil <Kamil@test.pl>
2018-01-16 22:01:50 +0100

    Create template for future development

---
+++ E-DictionaryBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_DictionaryBook.Startup))]
+namespace E_DictionaryBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
