0d916051194e09823ab85de0e0581123ed928da2
Kamil <Kamil@test.pl>
2018-01-25 14:04:35 +0100

    Add angular app to layout.cshtml, add Admin, Student,Teacher controllers and views.

---
+++ E-DictionaryBook/Content/Scripts/myTestApp.js
@@ -0,0 +1,11 @@
+﻿
+
+       var app = angular.module('myTestApp', []);
+        app.controller('myTestController', myFunction);
+
+        function myFunction($scope) {
+            $scope.firstName = "John";
+            $scope.lastName = "Doe";
+        }    
+    
+
